ea477b695ae841b0fd6400ac71d9989ea3f8d61a
mbouzouita <mbouzouita@gmail.com>
2020-03-30 01:58:38 +0200

    Add a middleware to catch non founded services.

--- app.js
+++ app.js
@@ -13,6 +13,13 @@ app.use(cors());
 const api = require('./api/v1/index');
 app.use(api);
 
+// If the http query is not found
+app.use((req, result) =>  {
+	const err = new Error('404 - Service not found !');
+	err.status = 404;
+	result.status(404).json(err);
+});
+
 // Start
 app.listen(app.get('port'), () => {
 	console.log(`Node server started at port ${app.get('port')}`);
